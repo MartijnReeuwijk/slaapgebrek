4514eb06c50cfff83d8d01717e3a1b9657c3690a
Martijn <m.reeuwijk@Live.nl>
2017-03-18 00:02:40 +0100

    Rest site

--- js/js.js
+++ js/js.js
@@ -1,8 +1,17 @@
 console.log("Hi");
 
-$(document).ready(function(){
-  $("nav").on('change',function(){
-    var project = "#" + $(this).val();
-    $.scrollTo($(project), 1000);
+// magic
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
